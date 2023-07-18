2de5e0048933ccd9dbf27288a3705b41e5df2698
BrucelD14 <samuderaduta144@gmail.com>
2023-07-18 16:10:31 +0700

    menambah catatan dan admin

--- login.php
+++ login.php
@@ -9,7 +9,21 @@ if (isset($_POST['submit'])) {
 	$password = $_POST['password'];
 
 	$login = mysqli_query($conn, "SELECT * FROM admin WHERE username = '{$username}' AND password = '{$password}'");
-	
+
+	// admin'-- 
+	// yyy
+
+	// yyy' OR 1=1 -- 
+	// xxx
+
+	// yyy' OR 1=1 LIMIT 1 -- 
+	// xxx
+
+	// SELECT * FROM admin WHERE username = 'xxx' OR 1=1 LIMIT 1 -- ' AND password = 'yyy'
+
+	// admin'--
+	// yyy
+
 
 	if (mysqli_num_rows($login) == 0) {
 		die("username atau password yang anda masukan salah !!");
@@ -17,7 +31,6 @@ if (isset($_POST['submit'])) {
 		$_SESSION['admin'] = 1;
 		header("Location: ./admin.php");
 	}
-	
 }
 
 ?>
@@ -28,35 +41,36 @@ if (isset($_POST['submit'])) {
 <head>
 	<title>LOGIN | BLOG </title>
 	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-		<style>
-div {
-  width: 320px;
-  padding: 50px;
-  border: 3px solid black;
-  margin: 200px;
-}
-		</style>	
+	<style>
+		div {
+			width: 320px;
+			padding: 50px;
+			border: 3px solid black;
+			margin: 200px;
+		}
+	</style>
 </head>
 
 <body>
-	<center><div>
-	<h1 style="text-align: center">panel login</h1>
+	<center>
+		<div>
+			<h1 style="text-align: center">panel login</h1>
 
-	
-	<form action="" method="post" style="text-align: center">
-	
-		<label>Username:</label>
-		<input type="text" name="username">
-		<br/><br/>
-		<label>Password:</label>
-		<input type="password" name="password">
-		
-		<br>
-		<br>
-		<input type="submit" name="submit" value="Login">
-	
-	</form>
-	</div>
+
+			<form action="" method="post" style="text-align: center">
+
+				<label>Username:</label>
+				<input type="text" name="username">
+				<br /><br />
+				<label>Password:</label>
+				<input type="password" name="password">
+
+				<br>
+				<br>
+				<input type="submit" name="submit" value="Login">
+
+			</form>
+		</div>
 </body>
 
 </html>
